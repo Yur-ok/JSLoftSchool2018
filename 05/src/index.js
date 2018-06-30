9e61b449c1dd7fad0f4719353da4fa18042483ed
Yuriy <Materik-r@yandex.ru>
2018-06-30 16:13:35 +0700

    сделал ВСЕ задачи кроме последней

---
+++ 05/src/index.js
@@ -0,0 +1,113 @@
+/* ДЗ 3 - работа с массивами и объеектами */
+
+/*
+ Задача 1:
+ Напишите аналог встроенного метода forEach для работы с массивами
+ */
+function forEach(array, fn) {
+    for (let i = 0; i < array.length; i++) {
+        fn(array[i]);
+    }
+}
+
+/*
+ Задача 2:
+ Напишите аналог встроенного метода map для работы с массивами
+ */
+function map(array, fn) {
+    let resArr = [];
+    for (let i = 0; i < array.length; i++) {
+        resArr[i] = fn(array[i]);
+    }
+    return resArr;
+}
+
+/*
+ Задача 3:
+ Напишите аналог встроенного метода reduce для работы с массивами
+ */
+function reduce(array, fn, initial) {
+    let res = 0;
+    for (let i = initial; i < array.length; i++) {
+        res = fn(array[i]);
+    }
+    return res;
+}
+
+/*
+ Задача 4:
+ Функция принимает объект и имя свойства, которое необходиом удалить из объекта
+ Функция должна удалить указанное свойство из указанного объекта
+ */
+function deleteProperty(obj, prop) {
+    return delete obj[prop];
+}
+
+/*
+ Задача 5:
+ Функция принимает объект и имя свойства и возвращает true или false
+ Функция должна проверить существует ли укзаанное свойство в указанном объекте
+ */
+function hasProperty(obj, prop) {
+    return obj.hasOwnProperty(prop);
+}
+
+/*
+ Задача 6:
+ Функция должна получить все перечисляемые свойства объекта и вернуть их в виде массива
+ */
+function getEnumProps(obj) {
+    return Object.keys(obj);
+}
+
+/*
+ Задача 7:
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистра и вернуть в виде массива
+ */
+function upperProps(obj) {
+    let props = [];
+    for (let p in obj) {
+        props.push(p.toUpperCase());
+    }
+    return props;
+}
+
+/*
+ Задача 8 *:
+ Напишите аналог встроенного метода slice для работы с массивами
+ */
+function slice(array, from, to) {
+    let res = [];
+    if (to > array.length || !to) {
+        to = array.length;
+    }
+    // res.length = to - from;
+    for (let i = from, j = 0; i < to; i++, j++) {
+        res[j] = array[i];
+    }
+    return res;
+}
+
+let ark = [1, 3, 4, 5, 3, 4, 53, 0];
+console.log(slice(ark, 3, 6));
+
+
+/*
+ Задача 9 *:
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+ */
+function createProxy(obj) {
+}
+
+export {
+    forEach,
+    map,
+    reduce,
+    deleteProperty,
+    hasProperty,
+    getEnumProps,
+    upperProps,
+    slice,
+    createProxy
+};
